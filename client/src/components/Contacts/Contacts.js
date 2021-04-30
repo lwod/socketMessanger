992a78395fb85ceb0cd44889fa55f36a35f12592
lwod <lwodcurrentmail@gmail.com>
2021-04-30 10:59:33 +0300

    add new contact

--- client/src/components/Contacts/Contacts.js
+++ client/src/components/Contacts/Contacts.js
@@ -1,11 +1,25 @@
 import React from 'react'
+import {useContacts} from "../../contexts/ContactsContext";
+
+import {ListGroup} from 'react-bootstrap'
 
 const Contacts = () => {
 	
-	return(
-		<div>
-			Contacts
-		</div>
+	const {contacts} = useContacts()
+	
+	return (
+		<>
+			<ListGroup variant={'flush'}>
+				{contacts.map(contact => {
+					return <ListGroup.Item
+						key={contact.id}
+						style={{color:'black'}}
+					>
+						{contact.name}
+					</ListGroup.Item>
+				})}
+			</ListGroup>
+		</>
 	)
 }
 
